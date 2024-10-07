5ca3a7ad6ef3c8c7ab478a710f40354cf629f411
mj.kim <alswo9853@gmail.com>
2024-10-07 18:44:35 +0900

    chore

--- backend/src/main/java/kr/ac/kopo/travel_home/domain/user/domain/dto/RatePerMonth.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.travel_home.domain.user.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class RatePerMonth {
-    private String month;
-    private double rate;
-}
--- backend/src/main/java/kr/ac/kopo/travel_home/domain/user/domain/dto/TravelInfo.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.kopo.travel_home.domain.user.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class TravelInfo {
-    private String code;
-    private String codeName;
-}
